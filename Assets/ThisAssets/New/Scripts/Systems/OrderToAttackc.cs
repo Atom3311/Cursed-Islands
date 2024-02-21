5f89c40873c62a6740d68cda0d0a297b2ef6397d
AndrewGitHaber <barsuk0608@mail.ru>
2024-02-21 09:02:50 +0600

    Выделение выбранного врага для атаки

--- Assets/ThisAssets/New/Scripts/Systems/OrderToAttackc.cs
+++ Assets/ThisAssets/New/Scripts/Systems/OrderToAttackc.cs
@@ -1,15 +1,46 @@
 ﻿using Unity.Entities;
+using Unity.Collections;
+using Unity.Transforms;
+
 [UpdateAfter(typeof(OrdersController))]
 [UpdateAfter(typeof(EnemiesHealthControll))]
+[UpdateAfter(typeof(InputHandler))]
 public partial struct OrderToAttackc : ISystem
 {
     private void OnCreate(ref SystemState state)
     {
         state.RequireForUpdate<OrderInformation>();
+        state.RequireForUpdate<InformationAboutInputPlayer>();
+        state.RequireForUpdate<GraphicSettingsComponent>();
     }
     private void OnUpdate(ref SystemState state)
     {
+        var ecb = new EntityCommandBuffer(Allocator.Temp);
+
+        var input = SystemAPI.GetSingleton<InformationAboutInputPlayer>();
+        var graphicSettings = SystemAPI.GetSingleton<GraphicSettingsComponent>();
+
+        if (input.ClickDown)
+        {
+            if(SystemAPI.TryGetSingletonEntity<GraphicOfChooseEnemy>(out Entity graphic))
+            {
+                ecb.DestroyEntity(graphic);
+            }
+                
+        }
+
         OrderInformation orderInformation = SystemAPI.GetSingleton<OrderInformation>();
+
+        Entity targetEntity = orderInformation.TargetEntity;
+
+        if (orderInformation.DuringOrder == Order.None)
+        {
+            ecb.Playback(state.EntityManager);
+            return;
+        }
+
+        bool successfullOrder = orderInformation.DuringOrder == Order.Attack;
+
         foreach (var(battleComponent, healthState, choosed) in SystemAPI.Query<
             RefRW<BattleComponent>,
             RefRO<HealthState>,
@@ -20,17 +51,22 @@ public partial struct OrderToAttackc : ISystem
                 battleComponent.ValueRW.Target = Entity.Null;
                 continue;
             }
-            if (orderInformation.DuringOrder == Order.None)
-                return;
-
-            if (orderInformation.DuringOrder != Order.Attack)
+            if (!successfullOrder)
             {
                 battleComponent.ValueRW.Target = Entity.Null;
                 continue;
             }
 
-            Entity targetEntity = orderInformation.TargetEntity;
             battleComponent.ValueRW.Target = targetEntity;
+
         }
+
+        if (successfullOrder)
+        {
+            Entity createdGraphic = ecb.Instantiate(graphicSettings.GraphicOfChooseEnemy);
+            ecb.AddComponent<GraphicOfChooseEnemy>(createdGraphic);
+            ecb.AddComponent(createdGraphic, new Parent() { Value = targetEntity });
+        }
+        ecb.Playback(state.EntityManager);
     }
 }
