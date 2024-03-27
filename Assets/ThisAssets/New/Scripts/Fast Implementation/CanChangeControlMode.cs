4bbbd06ee275fa63fa1b8854a9be36956831d40e
1xbuilder <132667670+1xbuilder@users.noreply.github.com>
2024-03-27 20:51:30 +0600

    чето новое

--- Assets/ThisAssets/New/Scripts/Fast Implementation/CanChangeControlMode.cs
+++ Assets/ThisAssets/New/Scripts/Fast Implementation/CanChangeControlMode.cs
@@ -4,9 +4,16 @@ using UnityEngine.UI;
 public class CanChangeControlMode : MonoBehaviour
 {
     public Button ThisButton;
+    public Image ImageButton;
     public ControlMode TargetMode;
     private void Awake()
     {
         ThisButton = GetComponent<Button>();
+        ImageButton = GetComponent<Image>();
+    }
+
+    public void Colorrrr()
+    {
+        ImageButton.color = Color.red;
     }
 }
--- Assets/ThisAssets/New/Scripts/Monobehavor/TextWithControlMode.cs
+++ Assets/ThisAssets/New/Scripts/Monobehavor/TextWithControlMode.cs
@@ -6,15 +6,21 @@ public class TextWithControlMode : MonoBehaviour
     public string StartText;
     private string[] _namesOfControlModes = new string[3];
     private Text _textComponent;
+    private Image _imageComponent;
+    private Vector4 Color;
     private void Awake()
     {
         _namesOfControlModes[(int)ControlMode.Order] = "Приказы";
         _namesOfControlModes[(int)ControlMode.Selection] = "Выбор";
         _namesOfControlModes[(int)ControlMode.Viewing] = "Просмотр";
         _textComponent = GetComponent<Text>();
+        _imageComponent = GetComponent<Image>();
     }
     public void WriteText(ControlMode mode)
     {
         _textComponent.text = StartText + _namesOfControlModes[(int)mode];
+
+        Color = new Vector4(255, 0, 255 , 1);
+        _imageComponent.color = Color;
     }
 }
